ce4c20439bff6e8f9b85fdd36fec62f07b4b5819
Phil Smith <philisweatly@gmail.com>
2020-05-23 12:49:45 -0500

    media player

---
+++ main.js
@@ -0,0 +1,63 @@
+var myAudio = document.getElementById("player");
+var isPlaying = false;
+var playPause = document.getElementById('play-pause');
+var icon = document.getElementById('icon');
+var player = document.getElementById('player');
+
+function togglePlay() {
+  if (isPlaying) {
+    myAudio.pause()
+  } else {
+    myAudio.play();
+  }
+};
+
+myAudio.onplaying = function() {
+  isPlaying = true;
+    icon.classList.remove('fas', 'fa-play');
+    icon.classList.add('fas', 'fa-pause');
+
+};
+
+myAudio.onpause = function() {
+  isPlaying = false;
+    icon.classList.remove('fas', 'fa-pause');
+    icon.classList.add('fas', 'fa-play');
+
+};
+
+
+
+
+var nextTrack = document.getElementById('next-track')
+var previousTrack = document.getElementById('previous-track')
+var i = 1
+
+nextTrack.addEventListener('click', function() {
+    var tracklist = [
+        "./sounds/track1.mp3",
+        "./sounds/track2.mp3",
+        "./sounds/track3.mp3",
+        "./sounds/track4.mp3",
+        "./sounds/track5.mp3",
+        "./sounds/track6.mp3",
+        "./sounds/track7.mp3",
+        "./sounds/track8.mp3"
+    ]
+    
+    
+    myAudio.src = tracklist[i];
+    myAudio.play();
+    i++
+
+    previousTrack.addEventListener('click', function() {
+       i--;
+        myAudio.src = tracklist[i];
+        myAudio.play();    
+    });
+});
+
+
+
+    
+
